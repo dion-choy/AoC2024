fee2a6760cc9a1bd1234ead594cd58694484bd9f
dion-choy <dionchoynj@gmail.com>
2024-12-05 15:50:44 +0800

    AoC 2024 day 5

---
+++ day5/part1.php
@@ -0,0 +1,55 @@
+<?php
+define("orderLen", 1176);
+$input = fopen("input.txt", "r", true);
+$smaller = [];
+$bigger = [];
+
+for ($i=0; $i < orderLen; $i++) { 
+    $tempArr = explode("|", trim(fgets($input)));
+
+    if(!isset($bigger[$tempArr[0]])) {
+        $bigger[$tempArr[0]] = [];
+    }
+    array_push($bigger[$tempArr[0]], $tempArr[1]);
+
+    if(!isset($smaller[$tempArr[1]])) {
+        $smaller[$tempArr[1]] = [];
+    }
+    array_push($smaller[$tempArr[1]], $tempArr[0]);
+}
+
+fgets($input);
+
+$updates = [];
+while(!feof($input)) {
+    array_push($updates, explode(",", trim(fgets($input))));
+}
+
+
+$sum = 0;
+foreach ($updates as $update) {
+    $orderedFlag = true;
+
+    for ($i=0; $i < count($update); $i++) { 
+        $cur = $update[$i];
+        $left = array_slice($update, 0, $i);
+        $right = array_slice($update, $i+1, count($update));
+
+        if(isset($bigger[$cur]) && array_intersect($right, $bigger[$cur]) != $right) {
+            $orderedFlag = false;
+            break;
+        }
+
+        if(isset($smaller[$cur]) && array_intersect($left, $smaller[$cur]) != $left) {
+            $orderedFlag = false;
+            break;
+        }
+        
+    }
+
+    if($orderedFlag) {
+        $sum += $update[(count($update)-1)/2];
+    }
+}
+
+echo $sum;
---
+++ day5/part2.php
@@ -0,0 +1,71 @@
+<?php
+define("orderLen", 1176);
+$input = fopen("input.txt", "r", true);
+$smaller = [];
+
+for ($i=0; $i < orderLen; $i++) { 
+    $tempArr = explode("|", trim(fgets($input)));
+
+    if(!isset($bigger[$tempArr[0]])) {
+        $bigger[$tempArr[0]] = [];
+    }
+    array_push($bigger[$tempArr[0]], $tempArr[1]);
+
+    if(!isset($smaller[$tempArr[1]])) {
+        $smaller[$tempArr[1]] = [];
+    }
+    array_push($smaller[$tempArr[1]], $tempArr[0]);
+}
+
+fgets($input);
+
+$updates = [];
+while(!feof($input)) {
+    array_push($updates, explode(",", trim(fgets($input))));
+}
+
+
+$sum = 0;
+foreach ($updates as $update) {
+    $orderedFlag = true;
+
+    for ($i=0; $i < count($update); $i++) { 
+        $cur = $update[$i];
+        $left = array_slice($update, 0, $i);
+        $right = array_slice($update, $i+1, count($update));
+
+        if(isset($bigger[$cur]) && array_intersect($right, $bigger[$cur]) != $right) {
+            $orderedFlag = false;
+            break;
+        }
+
+        if(isset($smaller[$cur]) && array_intersect($left, $smaller[$cur]) != $left) {
+            $orderedFlag = false;
+            break;
+        }
+        
+    }
+
+    if(!$orderedFlag) {
+        $sum += reorder($update)[(count($update)-1)/2];
+    }
+}
+
+echo $sum;
+
+function reorder($update) {
+    global $smaller;
+
+    for ($i=0; $i < count($update); $i++) { 
+        $cur = $update[$i];
+        $left = array_slice($update, 0, $i);
+
+        if(isset($smaller[$cur]) && array_intersect($left, $smaller[$cur]) != $left) {
+            $min = min(array_keys(array_diff($left, array_intersect($left, $smaller[$cur]))));
+            $moveNum = array_splice($update, $i, 1);
+            array_splice($update, $min, 0, $moveNum);
+        }
+    }
+
+    return $update;
+}
