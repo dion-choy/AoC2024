66b1a3512ee84b221870f2a7406511804cb2cc8a
dion-choy <dionchoynj@gmail.com>
2024-12-07 22:42:33 +0800

    readded lines of code for edge cases

--- php/day5/part2.php
+++ php/day5/part2.php
@@ -2,9 +2,15 @@
 define("orderLen", 1176);
 $input = fopen("day5.txt", "r");
 $smaller = [];
+$bigger = [];
 
 for ($i=0; $i < orderLen; $i++) { 
     $tempArr = explode("|", trim(fgets($input)));
+
+    if(!isset($bigger[$tempArr[0]])) {
+        $bigger[$tempArr[0]] = [];
+    }
+    array_push($bigger[$tempArr[0]], $tempArr[1]);
 
     if(!isset($smaller[$tempArr[1]])) {
         $smaller[$tempArr[1]] = [];
@@ -28,6 +34,11 @@ foreach ($updates as $update) {
         $cur = $update[$i];
         $left = array_slice($update, 0, $i);
         $right = array_slice($update, $i+1, count($update));
+
+        if(isset($bigger[$cur]) && array_intersect($right, $bigger[$cur]) != $right) {
+            $orderedFlag = false;
+            break;
+        }
 
         if(isset($smaller[$cur]) && array_intersect($left, $smaller[$cur]) != $left) {
             $orderedFlag = false;
