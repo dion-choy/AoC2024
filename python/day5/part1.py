9a2e2fa9a353bc9f0a645da620be457676e47c86
dion-choy <dionchoynj@gmail.com>
2024-12-07 23:34:22 +0800

    added lines for edge cases

--- python/day5/part1.py
+++ python/day5/part1.py
@@ -1,13 +1,17 @@
-ORDERLEN = 1176
+ORDERLEN = 21;
 
 def main():
 
     smaller = {}
+    bigger = {}
     pages = []
-    with open("./day5.txt", "r", ) as file:
+    with open("./dayTest.txt", "r", ) as file:
         for num, line in enumerate(file):
             if (num < ORDERLEN):
                 tempList = line.strip().split("|")
+
+                bigger.setdefault(int(tempList[0]), set())
+                bigger[int(tempList[0])].add(int(tempList[1]))
 
                 smaller.setdefault(int(tempList[1]), set())
                 smaller[int(tempList[1])].add(int(tempList[0]))
@@ -17,7 +21,9 @@ def main():
     total = 0
     for row in pages:
         for i, num in enumerate(row):
-            if num not in smaller.keys() or not smaller[num].issuperset(row[0:i]):
+            if num in bigger.keys() and not bigger[num].issuperset(row[i+1:]):
+                break
+            if num in smaller.keys() and not smaller[num].issuperset(row[0:i]):
                 break
         else:
             total += row[int(len(row)/2)]
