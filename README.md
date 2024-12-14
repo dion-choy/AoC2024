eed8bf11b2cef8283baedfb407c73e016e2fa79a
dion-choy <dionchoynj@gmail.com>
2024-12-14 12:40:05 +0800

    day 13 aoc php

--- README.md
+++ README.md
@@ -16,8 +16,8 @@
 | [10](https://adventofcode.com/2024/day/10) |     ✅     |     ✅     |               |               |
 | [11](https://adventofcode.com/2024/day/11) |     ✅     |     ✅     |               |               |
 | [12](https://adventofcode.com/2024/day/12) |     ✅     |     ✅     |               |               |
+| [13](https://adventofcode.com/2024/day/13) |     ✅     |     ✅     |               |               |
 
-<!-- | [13](https://adventofcode.com/2024/day/13) |     ✅     |     ✅     |      ✅       |      ✅       | -->
 <!-- | [14](https://adventofcode.com/2024/day/14) |     ✅     |     ✅     |      ✅       |      ✅       | -->
 <!-- | [15](https://adventofcode.com/2024/day/15) |     ✅     |     ✅     |      ✅       |      ✅       | -->
 <!-- | [16](https://adventofcode.com/2024/day/16) |     ✅     |     ✅     |      ✅       |      ✅       | -->
---
+++ php/day13/part1.php
@@ -0,0 +1,32 @@
+<?php
+$input = fopen("day13.txt", "r");
+
+$machines = [];
+while(!feof($input)) {
+    $tempArr1 = [];
+    $tempArr2 = [];
+    $tempArr3 = [];
+    preg_match_all('/[0-9]{1,}/', fgets($input), $tempArr1);
+    preg_match_all('/[0-9]{1,}/', fgets($input), $tempArr2);
+    preg_match_all('/[0-9]{1,}/', fgets($input), $tempArr3);
+    array_push($machines, [...$tempArr1, ...$tempArr2, ...$tempArr3]);
+    fgets($input);
+}
+
+// [x1, x2][a] = xtarget
+// [y1, y2][b] = ytarget
+$total = 0;
+foreach ($machines as $machine) {
+    $machine[2][0] += 10000000000000;
+    $machine[2][1] += 10000000000000;
+
+    $invMat = [[$machine[1][1], -$machine[1][0]], [-$machine[0][1], $machine[0][0]]];
+    $detReci = 1/($machine[0][0]*$machine[1][1] - $machine[0][1]*$machine[1][0]);
+
+    $btnA = $detReci*($machine[1][1]*$machine[2][0] - $machine[1][0]*$machine[2][1]);
+    $btnB = $detReci*($machine[0][0]*$machine[2][1] - $machine[0][1]*$machine[2][0]);
+    if ("$btnA" == floor("$btnA") && "$btnB" == floor("$btnB")) {
+        $total += 3*$btnA + $btnB;
+    }
+}
+echo $total;
